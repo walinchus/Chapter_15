45b78f948599b2ead880f99a95d5d3d190774375
walinchus <Lucia@professionalnewsservices.com>
2017-04-26 10:39:46 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,23 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
-
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
-
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+import scraperwiki
+html = scraperwiki.scrape('http://inmo.ie/6022')
+import lxml.html
+root = lxml.html.fromstring(html) # turn our HTML into an lxml object
+tds = root.cssselect('td') # get all the <td tags
+for td in tds:
+print td.text_content() # just the text inside the HTML tag
+for td in tds:
+  record = { "td" : td.text_content() } # column name and value
+  scraperwiki.sqlite.save(["td"], record) # save the records one by one
+  print td.text_content() # just the text inside the HTML tag
+for td in tds:
+  record = { "td" : td.tag } # column name and value
+  scraperwiki.sqlite.save(["td"], record) # save the records one by one
+  print td.tag # just the text inside the HTML tag
+for td in tds:
+  record = { "td" : td.attrib['href'] } # column name and value
+  scraperwiki.sqlite.save(["td"], record) # save the records one by one
+  print td.td.attrib['href']# just the text inside the HTML tag
+for td in tds:
+  record = { "td" : td.tail } # column name and value
+  scraperwiki.sqlite.save(["td"], record) # save the records one by one
+  print td.tail
